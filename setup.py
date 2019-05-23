aa16564fc3df4720a62b7e39cc474a8acb9bf5ab
Steve Springett <steve@springett.us>
2019-05-22 21:22:02 -0500

    version bump

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='cyclonedx-bom',
-    version='0.2.0',
-    description='CycloneDX bill-of-material (BOM) generation utility',
+    version='0.3.0',
+    description='CycloneDX software bill-of-material (SBoM) generation utility',
     long_description=open("README.rst").read(),
     packages=find_packages(),
     package_data={'cyclonedx.schema': ['bom-1.0.xsd', 'spdx.xsd']},
