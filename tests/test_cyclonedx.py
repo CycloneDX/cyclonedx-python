56bbb40fe53bea4111b74d0564477305cca0053d
Patrick Dwyer <coderpatros@outlook.com>
2020-12-06 05:53:52 +1000

    Add test for invalid xml unicode characters

--- tests/test_cyclonedx.py
+++ tests/test_cyclonedx.py
@@ -61,3 +61,21 @@ def test_json_bom_generation():
         with open(os.path.join(dirname, 'bom.json'), 'rt') as f:
             actual_json = f.read()
         assert actual_json == expected_json
+
+def test_invalid_xml_characters():
+    with tempfile.TemporaryDirectory() as dirname:
+        # arrange
+        with open(os.path.join(script_path, 'resources', 'invalid-xml-characters', 'bom.xml'), 'r') as bf:
+            expected_xml = bf.read()
+        
+        # act
+        subprocess.check_output([
+            'cyclonedx-py',
+            '-i', os.path.join(script_path, 'resources', 'invalid-xml-characters', 'requirements.txt'),
+            '-o', os.path.join(dirname, 'bom.xml'),
+        ])
+
+        # assert
+        with open(os.path.join(dirname, 'bom.xml'), 'rt') as f:
+            actual_xml = f.read()
+        assert actual_xml == expected_xml
