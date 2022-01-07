0342712495fddfee6a994db719e49efd60ba8725
Paul Horton <phorton@sonatype.com>
2022-01-07 15:48:32 +0000

    doc: migration to RTD (#296)
    * doc: migration to RTD.
    
    Signed-off-by: Paul Horton <phorton@sonatype.com>
    
    * doc: removed references to schema version 1.4
    
    Signed-off-by: Paul Horton <phorton@sonatype.com>
    Signed-off-by: Paul Horton <paul.horton@owasp.org>

---
+++ docs/conf.py
@@ -0,0 +1,61 @@
+# encoding: utf-8
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+import pkg_resources
+
+# -- Project information -----------------------------------------------------
+
+project = 'CycloneDX Python Library'
+copyright = '2022, Copyright (c) OWASP Foundation'
+author = 'Paul Horton, Jan Kowalleck, Steve Springett, Patrick Dwyer'
+
+# The full version, including alpha/beta/rc tags
+release = pkg_resources.get_distribution("cyclonedx-bom").version
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "autoapi.extension",
+    "sphinx_rtd_theme"
+]
+
+# Document Python Code
+autoapi_type = 'python'
+autoapi_dirs = ['../cyclonedx_py']
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
