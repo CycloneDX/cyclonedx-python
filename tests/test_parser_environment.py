6a1ee6a0e91e81ba2a7c34003d025248d36f550c
Thomas Meckel <tmeckel@users.noreply.github.com>
2022-11-30 11:39:55 +0000

    Changes to tests/test_parser_environment.py: * added test_simple_use_location_filter * added test_simple_use_requiremets
    Signed-off-by: Thomas Meckel <tmeckel@users.noreply.github.com>

--- tests/test_parser_environment.py
+++ tests/test_parser_environment.py
@@ -16,7 +16,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (c) OWASP Foundation. All Rights Reserved.
-
+import os
+import sys
 from unittest import TestCase
 
 from cyclonedx.model import License, LicenseChoice
@@ -63,3 +64,37 @@ class TestEnvironmentParser(TestCase):
         self.assertEqual(len(c_tox.licenses), 2)
         self.assertEqual({LicenseChoice(license_=License(license_name="MIT License")),
                           LicenseChoice(license_=License(license_name="MIT"))}, c_tox.licenses)
+
+    def test_simple_use_location_filter(self) -> None:
+        # Ensure that it makes no difference if we pass the complete sys.path
+        # as location filter or no filter at all
+        parser_sys_path = EnvironmentParser(location_filter=sys.path)
+        self.assertGreater(parser_sys_path.component_count(), 1)
+
+        parser_no_filter = EnvironmentParser()
+        self.assertGreater(parser_no_filter.component_count(), 1)
+        self.assertEqual(parser_no_filter.component_count(), parser_sys_path.component_count())
+
+        # ensure that no components are found if we set the location filter
+        # to a non existing path
+        parser_invalid_filter = EnvironmentParser(location_filter=["/tmp/fa12b66cac07e4bb480e98eba9627737"])
+        self.assertEqual(parser_invalid_filter.component_count(), 0)
+
+    def test_simple_use_requiremets(self) -> None:
+        with open(os.path.join(os.path.dirname(__file__),
+                               'fixtures/requirements-simple.txt')) as r:
+            parser = EnvironmentParser(requirements_content=r.read())
+        components = parser.get_components()
+        self.assertEqual(1, len(components))
+
+        parser = EnvironmentParser(requirements_content=os.path.join(os.path.dirname(__file__),
+                                                                     'fixtures/requirements-simple.txt'))
+        components = parser.get_components()
+        self.assertGreaterEqual(1, len(components), f"{components}")
+
+        parser = EnvironmentParser(
+            location_filter=["/tmp/fa12b66cac07e4bb480e98eba9627737"],
+            requirements_content=os.path.join(os.path.dirname(__file__),
+                                              'fixtures/requirements-simple.txt'))
+        components = parser.get_components()
+        self.assertGreaterEqual(0, len(components))
