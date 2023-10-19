6714ae33b178ad2fccc73609b583a2f168c1bc55
Jan Kowalleck <jan.kowalleck@gmail.com>
2023-10-19 14:43:11 +0200

    adjust dep boundaries of `chardet`
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- deps.lowest.r
+++ deps.lowest.r
@@ -5,6 +5,7 @@ cyclonedx-python-lib == 2.0.0
 packageurl-python == 0.9.0
 importlib-metadata == 3.4.0 # ; python_version < '3.8'
 pip-requirements-parser == 32.0.0
+chardet == 5.0.0
 setuptools == 47.0.0
 types-setuptools == 57.0.0
 toml == 0.10.0
