1942e094c80bc3053d3ea3f4e68600666b25e667
Jan Kowalleck <jan.kowalleck@gmail.com>
2023-10-25 20:10:35 +0200

    refactor: cli properly used
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- cyclonedx_py/__main__.py
+++ cyclonedx_py/__main__.py
@@ -15,6 +15,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (c) OWASP Foundation. All Rights Reserved.
 
-from ._internal.client import main
+from ._internal.cli import main
 
 main(prog_name=__package__)
