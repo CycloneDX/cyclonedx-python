bba3c4c48d92980bdf60615533e38827b0607283
Jan Kowalleck <jan.kowalleck@gmail.com>
2023-10-27 19:26:26 +0200

    poc
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- cyclonedx_py/_internal/cli.py
+++ cyclonedx_py/_internal/cli.py
@@ -59,7 +59,7 @@ class CS_bar(CS_BomBuilder):
     def make_argument_parser(**kwargs: Any) -> argparse.ArgumentParser:
         p = argparse.ArgumentParser(description='description CS_bar TODO', **kwargs)
         p.add_argument('-i', '--infile', help='I HELP TODO', nargs=argparse.ONE_OR_MORE,
-                       type=argparse.FileType('rb'), default=[stdin.buffer])
+                       type=argparse.FileType('rb'), default='-')
         return p
 
     def __call__(self) -> Bom:
@@ -72,30 +72,41 @@ class Command:
     def make_argument_parser(**kwargs: Any) -> argparse.ArgumentParser:
         BooleanOptionalAction: Optional[argparse.Action] = getattr(argparse, 'BooleanOptionalAction', None)
 
+        def mk_OutputFormatCI(value: str) -> OutputFormat:
+            try:
+                return OutputFormat[value.upper()]
+            except KeyError:
+                raise argparse.ArgumentTypeError(f'unsupported value {value!r}')
+
         p = argparse.ArgumentParser(
             description='description TODO',
             formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            allow_abbrev=False,
             **kwargs)
         p.add_argument('--version', action='version', version=__version__)
         sp = p.add_subparsers(metavar='command', required=True)
 
-        op = argparse.ArgumentParser(add_help=False, formatter_class=p.formatter_class)
+        op = argparse.ArgumentParser(add_help=False)
         op.add_argument('-o', '--outfile',
                         help='O HELP TODO',
                         dest='outfile',
                         type=argparse.FileType('w', encoding='utf8'),
                         default='-')
         op.add_argument('--schema-version',
+                        help='SV TODO'
+                             f'\n{{choice: {", ".join(sorted((v.to_version() for v in SchemaVersion), reverse=True))}}}',
+                        metavar='',
                         dest='schemaVersion',
-                        metavar='SCHEMA_VERSION',
-                        choices=sorted((v.to_version() for v in SchemaVersion), reverse=True),
+                        choices=SchemaVersion,
                         type=SchemaVersion.from_version,
                         default=SchemaVersion.V1_4.to_version())
         op.add_argument('--output-format',
+                        help='OF TODO'
+                             f'\n{{choice: {", ".join(sorted(f.name for f in OutputFormat))}}}',
+                        metavar='FORMAT',
                         dest='outputFormat',
-                        metavar='OUTPUT_FORMAT',
-                        choices=sorted(f.name for f in OutputFormat),
-                        type=lambda v: OutputFormat[v.upper()],
+                        choices=OutputFormat,
+                        type=mk_OutputFormatCI,
                         default=OutputFormat.JSON.name)
         if BooleanOptionalAction:
             op.add_argument('--validate',
@@ -123,12 +134,13 @@ class Command:
             ('foo', CS_foo, 'foo description'),
             ('bar', CS_bar, 'bar description'),
         ):
-            spp = scc.make_argument_parser(add_help=False, formatter_class=p.formatter_class)
+            spp = scc.make_argument_parser(add_help=False)
             sp.add_parser(sct,
                           help=scd,
                           description=spp.description,
                           parents=[spp, op],
-                          formatter_class=p.formatter_class
+                          formatter_class=p.formatter_class,
+                          allow_abbrev=p.allow_abbrev,
                           ).set_defaults(bbc=scc)
             del sct, scc, scd
 
@@ -138,21 +150,23 @@ class Command:
         self._args = args
 
     def __call__(self) -> int:
-        print('generating bom...', file=stderr)
+        print('DEBUG: args =', self._args, file=stderr)
+        print('Generating SBOM ...', file=stderr)
         try:
             bom = self._args.bbc(self._args)()
+        # TODO: expected error handling ...
         except Exception as error:
-            print('Unexpected Error:', error, file=stderr)
+            print(f'{error.__class__.__qualname__}:', error, file=stderr)
             return 1
-        print('generating bom result...', file=stderr)
+        print(f'Serializing SBOM: {self._args.schemaVersion.to_version()}/{self._args.outputFormat.name}', file=stderr)
         try:
             outputter = make_outputter(bom, self._args.outputFormat, self._args.schemaVersion)
             output = outputter.output_as_string(indent=2)
         except Exception as error:
-            print('Output Errors:', error, file=stderr)
+            print('Serialization Errors:', error, file=stderr)
             return 1
         if self._args.validate:
-            print('validating bom result...', file=stderr)
+            print('Validating ...', file=stderr)
             validation_error = make_schemabased_validator(
                 outputter.output_format,
                 outputter.schema_version
@@ -160,14 +174,14 @@ class Command:
             if validation_error is not None:
                 print('Validation Errors:', validation_error.data, file=stderr)
                 return 2
-            print('bom result valid.', file=stderr)
+            print('Valid.', file=stderr)
         else:
-            print('skipped validation.', file=stderr)
-        print('writing to', self._args.outfile.name, file=stderr)
+            print('Skipped validation.', file=stderr)
+        print('Writing to:', self._args.outfile.name, file=stderr)
         written = self._args.outfile.write(output)
         self._args.outfile.close()
         # start with a line break, to finalize possibly unterminated output.
-        print('\nwrote', written, 'bytes to', self._args.outfile.name, file=stderr)
+        print('\nWrote', written, 'bytes to', self._args.outfile.name, file=stderr)
         return 0
 
 
