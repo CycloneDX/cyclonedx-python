1c87e118f69d14ff8f548463fad484ef3e24c74b
Jan Kowalleck <jan.kowalleck@gmail.com>
2023-12-19 12:06:57 +0100

    tests
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- cyclonedx_py/_internal/pipenv.py
+++ cyclonedx_py/_internal/pipenv.py
@@ -65,7 +65,7 @@ class PipenvBB(BomBuilder):
                        help='both develop and default packages [env var: PIPENV_DEV]',
                        action='store_true',
                        dest='dev',
-                       default=getenv('PIPENV_DEV', '').lower() in ('1', 'true', 'yes'))
+                       default=getenv('PIPENV_DEV', '').lower() in ('1', 'true', 'yes', 'on'))
         p.add_argument('--pypi-mirror',
                        metavar='URL',
                        help='Specify a PyPI mirror [env var: PIPENV_PYPI_MIRROR]',
