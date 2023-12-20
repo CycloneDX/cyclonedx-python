d6289808b28e99f1b8052af35f87c4558f5e4f6b
Jan Kowalleck <jan.kowalleck@gmail.com>
2023-12-21 00:05:55 +0100

    tests
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

---
+++ tests/integration/test_environment.py
@@ -0,0 +1,49 @@
+# This file is part of CycloneDX Python Lib
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) OWASP Foundation. All Rights Reserved.
+
+
+import random
+from contextlib import redirect_stderr, redirect_stdout
+from glob import glob
+from io import StringIO
+from os.path import basename, dirname, join
+from typing import Any, Generator
+from unittest import TestCase
+
+from cyclonedx.schema import OutputFormat, SchemaVersion
+from ddt import ddt, named_data
+
+from cyclonedx_py._internal.cli import run as run_cli
+from tests import INFILES_DIRECTORY, SUPPORTED_OF_SV, SnapshotMixin, make_comparable
+
+
+initfiles = glob(join(INFILES_DIRECTORY, 'environment', '*', 'init.py'))
+projectdirs = list(dirname(initfile) for initfile in initfiles)
+
+test_data = tuple(
+    (f'{basename(projectdir)}-{sv.name}-{of.name}', projectdir, sv, of)
+    for projectdir in projectdirs
+    for of, sv in SUPPORTED_OF_SV
+)
+
+
+def test_data_file_filter(s: str) -> Generator[Any, None, None]:
+    return ((n, d, sv, of) for n, d, sv, of in test_data if s in n)
+
+
+class TestPipenv(TestCase, SnapshotMixin):
+    pass  # TODO
