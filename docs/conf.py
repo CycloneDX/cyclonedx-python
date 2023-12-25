1235348c3a9f3d28504e36134bbf30670b448b32
semantic-release <semantic-release>
2023-12-25 19:34:15 +0000

    chore(release): 4.0.0-rc.4
    Automatically generated by python-semantic-release
    
    Signed-off-by: semantic-release <semantic-release>

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ author = 'Paul Horton, Jan Kowalleck, Steve Springett, Patrick Dwyer'
 
 # The full version, including alpha/beta/rc tags
 # !! version is managed by semantic_release
-release = "4.0.0-rc.3"
+release = "4.0.0-rc.4"
 
 # -- General configuration ---------------------------------------------------
 
