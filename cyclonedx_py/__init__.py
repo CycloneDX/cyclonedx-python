c0253612417b1f529ca7af730b3c464253e4b4b4
semantic-release <semantic-release>
2023-12-25 17:59:05 +0000

    chore(release): 4.0.0-rc.1
    Automatically generated by python-semantic-release
    
    Signed-off-by: semantic-release <semantic-release>

--- cyclonedx_py/__init__.py
+++ cyclonedx_py/__init__.py
@@ -16,7 +16,7 @@
 # !! version is managed by semantic_release
 # do not use typing here, or else `semantic_release` might have issues finding the variable
 # flake8: noqa
-__version__ = "4.0.0-alpha.0"
+__version__ = "4.0.0-rc.1"
 
 # There is no stable/public API.
 # You may use this instead:
