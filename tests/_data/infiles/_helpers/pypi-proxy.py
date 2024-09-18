dddcb5dc6529e60c82dcfd756a0a8b31ae76e9bf
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-09-18 16:43:51 +0200

    fix: assert copyright headers (#787)
    utilizes flake8 plugin
    <https://pypi.org/project/flake8-copyright-validator/> to assert the
    correct headers
    
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tests/_data/infiles/_helpers/pypi-proxy.py
+++ tests/_data/infiles/_helpers/pypi-proxy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# This file is part of CycloneDX Python Lib
+# This file is part of CycloneDX Python
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
